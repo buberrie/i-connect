c27575321701de31f465cf604cf4dad7daeb19b1
Buberrie <83277559+buberrie@users.noreply.github.com>
2024-04-21 22:46:47 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
   
     window.addEventListener("load", handleLoad);
   
-    return () => window.removeEventListener("load", handleLoad);
+    // return () => window.removeEventListener("load", handleLoad);
   }, []);
   
 
